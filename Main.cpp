bcea1bcd5e57b7702a4dd156847e50f9d7df98af
TiffannyVarela <tiffanny_varela529@unitec.edu>
2019-08-27 20:54:20 -0600

    Prueba

--- Main.cpp
+++ Main.cpp
@@ -13,8 +13,35 @@
 #include "Usuario.cpp"
 #include "VOF.cpp"
 
+int main();
+
 int main()
 {
 	/* code */
+	return 0;
+}
+
+int menu(){
+	int opc=0;
+	while (true){
+		cout<<"MENU"<<endl
+			<<"1.-Hacer Clic"<<endl
+			<<"2.-Comprar Granja"<<endl
+			<<"3.-Comprar Banco"<<endl
+			<<"4.-Comprar Templo"<<endl
+			<<"5.-Comprar Oreo"<<endl
+			<<"6.-Comprar Migajas"<<endl
+			<<"7.-Mostrar Edificios y Mejoras Compradas"<<endl
+			<<"8.-Salir"<<endl;
+			
+		cout<<"Ingrese una opcion: ";
+		cin>>opc;
+		if(opc>=1 && opc<=8){
+			return opc;
+		}
+		else{
+			cout<<"Opcion elegida no es valida"<<endl;
+		}
+	}
 	return 0;
 }
